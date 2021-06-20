9c59e5737349ad20f4c3f54e22b0e92d6c1f7694
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-06-20 18:52:44 +0300

    class11

--- src/header.js
+++ src/header.js
@@ -10,7 +10,7 @@ class Header extends React.Component {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
           <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
+          <Link to="/Profile">Profile</Link>
           {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
       </Navbar>
     )
