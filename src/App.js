7db98512faa587471f5f51ac4524cf425c5ad40c
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-06-22 22:45:54 +0300

    trying

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React from 'react';
 import Header from './header';
-// import IsLoadingAndError from './IsLoadingAndError';
+import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './footer';
-import {withAuth0} from "@auth0/auth0-react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { withAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MyFavoriteBooks from './myFavoriteBooks';
 import Login from './Login';
-
 import Profile from './components/Profile';
 import LogoutButton from './components/LogoutButton';
-
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props)
+    console.log('app', this.props);
     const { isAuthenticated } = this.props.auth0;
-    return(
+    return (
       <>
         <Router>
           {/* <IsLoadingAndError> */}
             <Header />
-              <Switch>
-                <Route exact path="/">
-                  {/* TODO: if the Profile is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
-                  {isAuthenticated?<MyFavoriteBooks/>:<Login/>}
-                  {isAuthenticated?<LogoutButton/>:""}
-                  {/* {isAuthenticated?<LogoutButton/>:<Login/>} */}
-                </Route >
-                <Route exact path="/Profile">
-                {isAuthenticated?<Profile/>:<LogoutButton/>}
+            <Switch>
+              <Route exact path="/">
+                {/* TODO: if the Profile is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+                {isAuthenticated ? <MyFavoriteBooks /> : <Login />}
+                {isAuthenticated?<LogoutButton/>:""}
+              </Route>
+              <Route exact path="/Profile">
                 {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-                       
-
-                </Route>
-
-              </Switch>
+                {isAuthenticated?<Profile/>:<LogoutButton/>}
+              </Route>
+            </Switch>
             <Footer />
           {/* </IsLoadingAndError> */}
         </Router>
--- src/myFavoriteBooks.js
+++ src/myFavoriteBooks.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import './myFavoriteBooks.css';
 import BestBooks from './components/BestBooks';
-
+import './myFavoriteBooks.css';
 
 class MyFavoriteBooks extends React.Component {
   render() {
-    return(
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
-      <BestBooks/>
+        <BestBooks />
       </Jumbotron>
     );
   }
