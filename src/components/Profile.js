32f0b4f79f82e743216b9b031b84b2e6cb60173c
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-06-25 00:53:07 +0300

    class14

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Card from 'react-bootstrap/Card'
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -10,11 +11,21 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-     <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-    </div>
+    //  <div>
+    //     <img src={user.picture} alt={user.name} />
+    //     <h2>{user.name}</h2>
+    //     <p>{user.email}</p>
+    // </div>
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={user.picture} />
+  <Card.Body>
+    <Card.Title>{user.name}</Card.Title>
+    <Card.Text>
+    {user.email}
+    </Card.Text>
+    {/* <Button variant="primary">Go somewhere</Button> */}
+  </Card.Body>
+</Card>
     )
   );
 };
